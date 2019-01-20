a3a1cb1dc2a3f0235ff767a7efe456a0c8b13f09
Macbook <macbook@Macbooks-iMac.local>
2019-01-20 16:31:17 +0200

    More Work On Menu

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            StartGame();
+        }
+	}
+
+    void StartGame()
+    {
+        Vector3 beginPositionAt = new Vector3(0, 0, 0);
+        cameraTransform.transform.position = Vector3.Lerp(transform.position, beginPositionAt, 5.0f * Time.deltaTime);
+    }
+
+}
---
+++ Assets/SpawnObjectsMainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectsMainMenu : MonoBehaviour {
+
+    public GameObject objectOne;
+    public GameObject objectTwo;
+    public GameObject objectThree;
+    public GameObject objectFour;
+    GameObject clone;
+    GameObject[] gameObjectsToInstantiate = new GameObject[4];
+
+    int oldRandomNumber = -1;
+
+    // Use this for initialization
+    void Start () {
+
+        gameObjectsToInstantiate[0] = objectOne;
+        gameObjectsToInstantiate[1] = objectTwo;
+        gameObjectsToInstantiate[2] = objectThree;
+        gameObjectsToInstantiate[3] = objectFour;
+        InvokeRepeating("Spawn", 1.0f, 10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void Spawn()
+    {
+        int randomNumber = Random.Range(0, gameObjectsToInstantiate.Length);
+
+        if (randomNumber == oldRandomNumber)
+        {
+            randomNumber = Random.Range(0, gameObjectsToInstantiate.Length);
+            Spawn();
+            return;
+        }
+
+        clone = Instantiate(gameObjectsToInstantiate[randomNumber],
+                 transform.position, transform.rotation ) as GameObject;
+        oldRandomNumber = randomNumber;
+    }
+
+}
